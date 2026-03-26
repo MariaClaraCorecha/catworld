37a173ee5c4084d93ea6622e1d30bdde18012641
Maria Clara Corecha <mariaclaracorecha18@gmail.com>
2026-03-26 17:43:37 -0300

    Feat: Adicionando Navegação

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class NavbarComponent {
+  scrolled = false;
+  menuOpen = false;
+
+  //Detecta scroll para mudar o estilo
+  @HostListener('window:scroll')
+  onScroll(){
+    this.scrolled = window.scrollY > 50;
+
+  }
+
+  toggleMenu(){
+    this.menuOpen = !this.menuOpen;
+  }
+
+  //Scroll suave até seção por ID
+  scrollTo(sectionId: string) {
+    const el = document.getElementById(sectionId);
+    el?.scrollIntoView({ behavior: 'smooth' });
+    this.menuOpen = false;
+  }
+
+  links = [
+    {label: 'Raças', id: 'racas'},
+    {label: 'Cuidados', id: 'cuidados'},
+    {label: 'Alimentação', id: 'alimentação'},
+    {label: 'Exercícios', id: 'exercicios'},
+    {label: 'Curiosidades', id: 'curiosidades'},
+    {label: 'História', id: 'historia'},
+  ];
+}
