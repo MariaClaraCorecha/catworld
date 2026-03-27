1dde0963629b48ece4e8fca95c53de8b43a99c15
Maria Clara Corecha <mariaclaracorecha18@gmail.com>
2026-03-27 19:53:11 -0300

    Refactor: Alteração na sessão principal.

--- src/app/features/landing/hero/hero.ts
+++ src/app/features/landing/hero/hero.ts
@@ -2,30 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule,
-    MatButtonModule,
-    MatChipsModule
-  ],
+  imports: [CommonModule, MatButtonModule, MatChipsModule, MatIconModule],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
 export class HeroComponent {
-  //Template
   headline = 'Mundo dos Gatos';
   subtitle =
     'Descubra sobre temas como raças, cuidados, alimentação e muito mais sobre seus felinos favoritos';
 
-  //Tópicos que o site aborda
   topics = [
-    { icon: '🐈', label: 'Raças' },
-    { icon: '💊', label: 'Cuidados' },
-    { icon: '🍖', label: 'Alimentação' },
-    { icon: '🧶', label: 'Exercícios' },
-    { icon: '✨', label: 'Curiosidades' },
-    { icon: '📜', label: 'História' },
+    { icon: 'pets', label: 'Raças' },
+    { icon: 'medical_services', label: 'Cuidados' },
+    { icon: 'restaurant', label: 'Alimentação' },
+    { icon: 'directions_run', label: 'Exercícios' },
+    { icon: 'auto_awesome', label: 'Curiosidades' },
+    { icon: 'history_edu', label: 'História' },
   ];
 }
