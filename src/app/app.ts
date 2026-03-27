e5497023fd8a872f1ab9da469b73bc77e78b777b
Maria Clara Corecha <mariaclaracorecha18@gmail.com>
2026-03-27 17:07:03 -0300

    Feat: Adiciona navegação em History

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './features/landing/hero/hero';
+import { NavbarComponent } from './shared/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent],
-  template: ` <app-hero /> `,
+  imports: [HeroComponent, NavbarComponent],
+  template: ` <app-hero /> <app-navbar/>`,
 })
 export class AppComponent {
   
