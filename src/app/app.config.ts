355b7607bd4c56ae67d692759073c3ecc3b476c1
mariaclaracorecha <mariaclaracorecha@gmail.com>
2026-04-01 14:21:13 -0300

    hotfix: corrigindo dependência depreciada no NPM

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+// @ts-ignore
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes),
+    // @ts-ignore
+    provideAnimationsAsync(),
+  ],
 };
