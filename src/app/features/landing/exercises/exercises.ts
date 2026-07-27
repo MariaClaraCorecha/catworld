de70082d4e760c1c70d949a4d3c9e6049c6b8314
mariaclaracorecha <mariaclaracorecha@gmail.com>
2026-07-27 17:22:03 -0300

    Feat: implement Exercises component with service integration and detailed layout

--- src/app/features/landing/exercises/exercises.ts
+++ src/app/features/landing/exercises/exercises.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+import { ExercisesService } from '../../../core/services/exercises';
 
 @Component({
   selector: 'app-exercises',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatIconModule,
+    MatRippleModule,
+  ],
   templateUrl: './exercises.html',
   styleUrl: './exercises.scss',
 })
-export class Exercises {}
+export class Exercises {
+  private exercisesService = inject(ExercisesService);
+  private router = inject(Router);
+
+  exercises = this.exercisesService.getAll();
+
+  goToDetail(id: string) {
+    this.router.navigate(['/exercicios', id]);
+  }
+}
