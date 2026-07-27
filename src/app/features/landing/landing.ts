5476a68ed9eca84c19b4d27d027d705cc5173982
mariaclaracorecha <mariaclaracorecha@gmail.com>
2026-07-27 17:22:14 -0300

    Feat: update LandingComponent to include Exercises and Curiosities sections

--- src/app/features/landing/landing.ts
+++ src/app/features/landing/landing.ts
@@ -4,17 +4,29 @@ import { HistoryComponent } from './history/history';
 import { BreedsComponent } from './breeds/breeds';
 import { CareComponent } from './care/care';
 import { FeedingComponent } from './feeding/feeding';
+import { Exercises } from './exercises/exercises';
+import { Curiosities } from './curiosities/curiosities';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent, BreedsComponent, HistoryComponent, CareComponent, FeedingComponent],
+  imports: [
+    HeroComponent,
+    BreedsComponent,
+    HistoryComponent,
+    CareComponent,
+    FeedingComponent,
+    Exercises,
+    Curiosities,
+  ],
   template: `
     <app-hero />
     <app-breeds />
     <app-history />
     <app-care />
-    <app-feeding/>
+    <app-feeding />
+    <app-exercises />
+    <app-curiosities />
   `,
 })
 export class LandingComponent {}
